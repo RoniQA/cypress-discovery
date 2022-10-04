4b896568e27a70c9be2bf230bb81bf987d0b24c4
RoninQA PC <ronierisoncosta@gmail.com>
2022-10-04 11:00:43 -0300

    Refactoring the code to english

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -51,4 +51,53 @@ describe('Cadastro', ()=>{
         cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
 
     })
+
+    it('CPF incorreto', ()=>{
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app')
+
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var deliver = {
+            name: 'Ronierison Costa',
+            cpf: '123321456AA',
+            email: 'roninqa@hotmail.com',
+            whatsapp: '85999999999',
+            address: {
+                postalcode: '60862460',
+                street:'Rua Regina de Fátima',
+                number: '1234',
+                details: 'Casa A',
+                district: 'Passaré',
+                city_state: 'Fortaleza/CE',
+            },
+            delivery_method: 'Moto',
+            driver_license: 'cnh-digital.jpg'
+        }
+
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
+        cy.get('input[type=button][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(deliver.address.number)
+        cy.get('input[name="address-details"]').type(deliver.address.details)
+
+        cy.get('input[name="address"]').should('have.value', deliver.address.street)
+        cy.get('input[name="district"]').should('have.value', deliver.address.district)
+        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
+
+        cy.contains('.delivery-method li', deliver.delivery_method).click()
+
+        cy.get('input[accept^="image/*"]').attachFile('/images/' + deliver.driver_license)
+
+        cy.get('form button[type="submit"]').click()
+
+        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
+
+    })
 })
