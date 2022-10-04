a0291d10274d2605b14a12a8987cb2503467abd1
RoninQA PC <ronierisoncosta@gmail.com>
2022-10-04 12:21:11 -0300

    Importing page object

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,63 +1,37 @@
-
+import SignupPage from '../pages/SignupPage'
 
 describe('Cadastro', ()=>{
     it('Usuário deve se tornar um entregador', ()=>{
-        cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
 
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
-
-        var entregador = {
-            nome: 'Ronierison Costa',
+        var deliver = {
+            name: 'Ronierison Costa',
             cpf: '12332145665',
             email: 'roninqa@hotmail.com',
             whatsapp: '85999999999',
-            endereco: {
-                cep: '60862460',
-                rua:'Rua Regina de Fátima',
-                numero: '1234',
-                complemento: 'Casa A',
-                bairro: 'Passaré',
-                cidade_uf: 'Fortaleza/CE',
+            address: {
+                postal_code: '60862460',
+                street:'Rua Regina de Fátima',
+                number: '1234',
+                details: 'Casa A',
+                district: 'Passaré',
+                city_state: 'Fortaleza/CE',
             },
-            metodo_entrega: 'Moto',
-            cnh: 'cnh-digital.jpg'
+            deliver_method: 'Moto',
+            driver_license: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(entregador.nome)
-        cy.get('input[name="cpf"]').type(entregador.cpf)
-        cy.get('input[name="email"]').type(entregador.email)
-        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+        var signup = new SignupPage()
 
-        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
-        cy.get('input[type=button][value="Buscar CEP"]').click()
-
-        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
-        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
-
-        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
-        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
-        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
-
-        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
-
-        cy.get('input[accept^="image/*"]').attachFile('/images/' + entregador.cnh)
-
-        cy.get('form button[type="submit"]').click()
+        signup.go()
+        signup.fillform(deliver)
+        signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-
-        cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
+        signup.modalContentShouldBe(expectedMessage) 
 
     })
 
     it('CPF incorreto', ()=>{
-        cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
-
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
         var deliver = {
             name: 'Ronierison Costa',
@@ -75,6 +49,10 @@ describe('Cadastro', ()=>{
             delivery_method: 'Moto',
             driver_license: 'cnh-digital.jpg'
         }
+
+        var signup = new SignupPage()
+
+        signup.go()
 
         cy.get('input[name="name"]').type(deliver.name)
         cy.get('input[name="cpf"]').type(deliver.cpf)
