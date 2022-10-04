9455e76f342d0443b24b1d65693850c715a4cc24
RoninQA PC <ronierisoncosta@gmail.com>
2022-10-04 13:40:12 -0300

    Getting better the code

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -39,41 +39,22 @@ describe('Cadastro', ()=>{
             email: 'roninqa@hotmail.com',
             whatsapp: '85999999999',
             address: {
-                postalcode: '60862460',
+                postal_code: '60862460',
                 street:'Rua Regina de Fátima',
                 number: '1234',
                 details: 'Casa A',
                 district: 'Passaré',
                 city_state: 'Fortaleza/CE',
             },
-            delivery_method: 'Moto',
+            deliver_method: 'Moto',
             driver_license: 'cnh-digital.jpg'
         }
 
         var signup = new SignupPage()
 
         signup.go()
-
-        cy.get('input[name="name"]').type(deliver.name)
-        cy.get('input[name="cpf"]').type(deliver.cpf)
-        cy.get('input[name="email"]').type(deliver.email)
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
-
-        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
-        cy.get('input[type=button][value="Buscar CEP"]').click()
-
-        cy.get('input[name="address-number"]').type(deliver.address.number)
-        cy.get('input[name="address-details"]').type(deliver.address.details)
-
-        cy.get('input[name="address"]').should('have.value', deliver.address.street)
-        cy.get('input[name="district"]').should('have.value', deliver.address.district)
-        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click()
-
-        cy.get('input[accept^="image/*"]').attachFile('/images/' + deliver.driver_license)
-
-        cy.get('form button[type="submit"]').click()
+        signup.fillform(deliver)
+        signup.submit()
 
         cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
 
