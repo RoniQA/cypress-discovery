cdfaee2b93d3bbe8708fd65a9e46a099171f2e85
RoninQA PC <ronierisoncosta@gmail.com>
2022-11-04 18:45:11 -0300

    Working with fixtures to make a clean code

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,46 +1,17 @@
-import SignupPage from '../pages/SignupPage'
+import signup from '../pages/SignupPage'
 
-describe('Cadastro', ()=>{
+describe('Cadastro', () => {
 
-    // before(function() {
-    //     cy.log('Tudo aqui é executado uma única vez ANTES de TODOS os casos de testes')
-    // })
+    beforeEach(function() {
+        cy.fixture('deliver').then((d)=> {
+            this.deliver = d
+        })
+    }) 
 
-    // beforeEach(function() {
-    //     cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
-    // })
-
-    // after(function() {
-    //     cy.log('Tudo aqui é executado uma única vez DEPOIS de TODOS os casos de testes')
-    // })
-
-    // afterEach(function() {
-    //     cy.log('Tudo aqui é executado sempre DEPOIS de CADA caso de teste')
-    // })
-
-    it('Usuário deve se tornar um entregador', ()=>{
-
-        var deliver = {
-            name: 'Ronierison Costa',
-            cpf: '12332145665',
-            email: 'roninqa@hotmail.com',
-            whatsapp: '85999999999',
-            address: {
-                postal_code: '60862460',
-                street:'Rua Regina de Fátima',
-                number: '1234',
-                details: 'Casa A',
-                district: 'Passaré',
-                city_state: 'Fortaleza/CE',
-            },
-            deliver_method: 'Moto',
-            driver_license: 'cnh-digital.jpg'
-        }
-
-        var signup = new SignupPage()
+    it('Usuário deve se tornar um entregador', function() {
 
         signup.go()
-        signup.fillform(deliver)
+        signup.fillform(this.deliver.signup)
         signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
@@ -48,29 +19,10 @@ describe('Cadastro', ()=>{
 
     })
 
-    it('CPF incorreto', ()=>{
-
-        var deliver = {
-            name: 'Ronierison Costa',
-            cpf: '123321456AA',
-            email: 'roninqa@hotmail.com',
-            whatsapp: '85999999999',
-            address: {
-                postal_code: '60862460',
-                street:'Rua Regina de Fátima',
-                number: '1234',
-                details: 'Casa A',
-                district: 'Passaré',
-                city_state: 'Fortaleza/CE',
-            },
-            deliver_method: 'Moto',
-            driver_license: 'cnh-digital.jpg'
-        }
-
-        var signup = new SignupPage()
+    it('CPF incorreto', function() {
 
         signup.go()
-        signup.fillform(deliver)
+        signup.fillform(this.deliver.cpf_invalido)
         signup.submit()
 
         cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
